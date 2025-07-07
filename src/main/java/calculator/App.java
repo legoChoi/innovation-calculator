efd78fe7c58ef78c91e30f4603c6b791eb341a8c
legoChoi <chlx159@gmail.com>
2025-07-07 12:54:33 +0900

    level 1-4 연산 반복

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,39 +7,47 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int a = sc.nextInt();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int b = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char op = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char op = sc.next().charAt(0);
 
-        int result = 0;
+            int result = 0;
 
-        switch (op) {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case '*':
-                result = a * b;
-                break;
-            case '/':
-                if (b == 0) {
-                    System.out.println("0으로 나눌 수 없습니다.");
+            switch (op) {
+                case '+':
+                    result = a + b;
                     break;
-                }
-                result = a / b;
-                break;
-            default:
-                System.out.println("잘못된 연산자 입니다.");
-        }
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    if (b == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        break;
+                    }
+                    result = a / b;
+                    break;
+            }
 
-        System.out.println("결과: " + result);
+            System.out.println("결과: " + result);
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
+            sc.nextLine();
+            String command = sc.nextLine();
+
+            if (command.equals("exit")) {
+                break;
+            }
+        }
 
         sc.close();
     }
