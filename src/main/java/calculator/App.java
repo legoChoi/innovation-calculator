ec8f0fc5b3ba6b392b5570b3893baf3f84d05a03
legoChoi <chlx159@gmail.com>
2025-07-07 12:45:31 +0900

    level 1-3 연산 결과 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,5 +15,32 @@ public class App {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char op = sc.next().charAt(0);
+
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    break;
+                }
+                result = a / b;
+                break;
+            default:
+                System.out.println("잘못된 연산자 입니다.");
+        }
+
+        System.out.println("결과: " + result);
+
+        sc.close();
     }
 }
